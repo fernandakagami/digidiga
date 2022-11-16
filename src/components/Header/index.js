66e005181eb416ce5a1dda3650ea428970871a81
Fernanda <fernandakagami@gmail.com>
2022-11-15 22:35:36 -0300

    create project

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import NavigationHeader from "../NavigationHeader"
+import './Header.css'
+
+function Header () {
+    return (
+      <header className="App-header">
+        <h1><span>digi</span>diga</h1>
+        <NavigationHeader />
+      </header>
+    )
+}
+
+export default Header
---
+++ src/components/NavigationHeader/index.js
@@ -0,0 +1,15 @@
+import './NavigationHeader.css'
+
+function NavigationHeader() {
+    return (
+        <nav>
+            <ul>
+                <li><a href='/'>About us</a></li>
+                <li><a href='/'>Our project</a></li>            
+                <li><a href='/'>Clients</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavigationHeader
