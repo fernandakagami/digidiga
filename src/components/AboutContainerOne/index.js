a432893a012ef4f258dff5f09172ca9cc95b181e
Fernanda <fernandakagami@gmail.com>
2022-11-17 20:58:53 -0300

    update section two

--- src/components/AboutContainerOne/index.js
+++ src/components/AboutContainerOne/index.js
@@ -2,7 +2,7 @@ import './AboutContainerOne.css'
 
 function AboutContainerOne() {
     return (
-        <div className='abouttitle'>
+        <div className='about-title'>
             <small className='about'>about</small>
             <h3>digi<small className='diga'>diga</small></h3>
         </div>
