34b929074fad8c8d1ba33c00157d229e188ad3b4
Fernanda <fernandakagami@gmail.com>
2022-11-22 10:18:09 -0300

    update Section Six

--- src/components/SectionSix/index.js
+++ src/components/SectionSix/index.js
@@ -1,6 +1,21 @@
+import './SectionSix.css'
+import SectionSixContact from '../SectionSixContact';
+import SectionSixImage from '../SectionSixImage';
+
 function SectionSix() {
     return (
-        <section className='Section-Six'>            
+        <section className='Section-Six'>
+            <div className='Section-Six-Container'>
+                <div>
+                    <h3 className='Section-Six-Contact'>Contact Us</h3>
+                    <p className='Section-Six-Text'>Are you interested in working with us or just want to ask question?</p>                      
+                    <SectionSixContact />                    
+                </div>
+                <SectionSixImage />                             
+            </div>    
+            
+            
+
         </section>
     )
 }
---
+++ src/components/SectionSixImage/index.js
@@ -0,0 +1,11 @@
+import frame from '../../images/contact/frame.png'
+
+function SectionSixImage() {
+    return (
+        <div>
+            <img className='Frame' src={frame} alt='man with plants'></img>  
+        </div>
+    )
+}
+
+export default SectionSixImage
