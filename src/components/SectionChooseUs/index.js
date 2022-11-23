e67221c30c3757debba6b3ee126fa25ea834f8d7
Fernanda <fernandakagami@gmail.com>
2022-11-23 19:37:39 -0300

    update section choose us

--- src/components/SectionChooseUs/index.js
+++ src/components/SectionChooseUs/index.js
@@ -3,13 +3,15 @@ import girl from '../../images/girl.png'
 
 function SectionChooseUs() {
     return (
-        <div className='Section-Choose-Us'>
-            <img src={girl} alt='girl' className='Section-Choose-Us-Image'></img>
-            <div className='Section-Choose-Us-Text'>
-                <h5>Why Choose us?</h5>
-                <p><strong>Digidiga</strong> selalu menganggap client sebagai teman yang akan kami bantu untuk menyelesaikan masalahnya, kami akan bantu Anda karena kami mempunyai kelebihan seperti dibawah ini.</p>
-            </div>
-        </div>
+        <section className='Section-Choose-Us'>
+            <div className='Section-Choose-Us-Container'>
+                <img src={girl} alt='girl' className='Section-Choose-Us-Image'></img>
+                <div className='Section-Choose-Us-Text'>
+                    <h5>Why Choose us?</h5>
+                    <p><strong>Digidiga</strong> selalu menganggap client sebagai teman yang akan kami bantu untuk menyelesaikan masalahnya, kami akan bantu Anda karena kami mempunyai kelebihan seperti dibawah ini.</p>
+                </div>    
+            </div>            
+        </section>
         
     )
 }
