ae7a662b04841d360000736ce45075e519019364
Fernanda <fernandakagami@gmail.com>
2022-11-23 18:59:50 -0300

    Update Section Three Title

--- src/components/SectionThreeTitle/index.js
+++ src/components/SectionThreeTitle/index.js
@@ -3,8 +3,8 @@ import './SectionThreeTitle.css'
 function SectionThreeTitle() {
     return (
         <div className='Section-Three-Title'>
-            <h4 className='Recent-Text'>recent</h4>
-            <h3>project</h3>
+            <h4 className='Section-Three-Title-Recent'>recent</h4>
+            <h3 className='Section-Three-Title-Project'>project</h3>
         </div>
     )    
 }
