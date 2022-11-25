012f37c6d2844df9196cdeb775a34a188cdf39b7
Fernanda <fernandakagami@gmail.com>
2022-11-25 08:37:52 -0300

    update list Container

---
+++ src/components/ListContainer/index.js
@@ -0,0 +1,57 @@
+import './ListContainer.css'
+import painel1 from '../../images/painel/painel1.png'
+import painel2 from '../../images/painel/painel2.png'
+import { useRef } from 'react'
+import React, { useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+
+function ListContainer () {
+    const [slideNumber, setSlideNumber] = useState(0);
+    const listRef = useRef();
+
+    const handleClick = (direction) => {
+        let distance = listRef.current.getBoundingClientRect().x -100;
+        if (direction === "left" && slideNumber > 0) {
+            setSlideNumber(slideNumber - 1);
+            listRef.current.style.transform = `translateX(${500 + distance}px)`;
+        }
+        if (direction === "right" && slideNumber < 2) {
+            setSlideNumber(slideNumber + 1);
+            listRef.current.style.transform = `translateX(${-500 + distance}px)`;
+        }
+    };
+
+    return (                
+        <div className="ListContainer">                                    
+            <div className="MovieContainer" ref={listRef}>
+                <div className='ItemContainer'>
+                    <img src={painel1} alt='painel1' className='MainImage'></img>
+                    <p className='ImageTitle'>Sistem Evaluasi dan Laporan</p>
+                    <small className='ImageText'>Universitas Sultan Ageng Tirtayasa</small>
+                </div>
+                <div className='ItemContainer'>
+                    <img src={painel2} alt='painel2' className='MainImage'></img>
+                    <p className='ImageTitle'>Sistem Evaluasi dan Laporan</p>
+                    <small className='ImageText'>Universitas Sultan Ageng Tirtayasa</small>
+                </div>
+                <div className='ItemContainer'>
+                    <img src={painel1} alt='painel1' className='MainImage'></img>
+                    <p className='ImageTitle'>Sistem Evaluasi dan Laporan</p>
+                    <small className='ImageText'>Universitas Sultan Ageng Tirtayasa</small>
+                </div>
+            </div>
+            <div className='SliderArrowContainer'>
+                <div className="SliderArrow" onClick={() => handleClick("left")}>
+                    <FontAwesomeIcon icon={solid('arrow-left')} />
+                </div>
+                <div className="SliderArrow" onClick={() => handleClick("right")}>
+                    <FontAwesomeIcon icon={solid('arrow-right')} />
+                </div>
+            </div>            
+            <div className='Line'></div>
+        </div>
+    )
+}
+
+export default ListContainer
